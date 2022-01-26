bed843d7c2c506f4c0068491d81cd425f1e0daac
sarawaniolka <waniolka.sara@gmail.com>
2022-01-27 00:06:50 +0100

    checking if the keyword is in the list

--- Strings_check_word.py
+++ Strings_check_word.py
@@ -1,3 +1,24 @@
-### Takes a list of sentences and  checks if there is a given word.
+# Takes a list of documents (each document is a string) and a keyword.
+#  Returns True if a keyword is found, False when it's not.
 
-print('test')
+    # Example:
+    # doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+    # >>> word_search(doc_list, 'casino')
+    # >>> True
+
+
+def word_search(doc_list, keyword):
+    x = False
+    for sentense in doc_list:
+        sentense = sentense.split(sep=' ')
+        for word in sentense:
+            word = word.strip().lower()
+            if word[-1] in ['.',',','!','?']:
+                word = word[:-1]
+            if word == keyword:
+                x = True
+            x = x or x
+    print(x)
+
+doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+word_search(doc_list,'casino')
